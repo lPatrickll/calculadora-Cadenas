b8cd9eb45c466b053b1f0be0ebdf47f5bde443e0
Patrick <patrickgonzalesquintana@gmail.com>
2024-05-13 22:47:32 -0400

    Se añadio la funcion sumarCadena que suma los numeros de una cadena

---
+++ src/calcCadenas.spec.js
@@ -0,0 +1,9 @@
+describe("Calculadora de Cadenas", () => {
+    it("deberia no mostrar nada ya que la cadena es vacia", () => {
+        expect(calculadoraCadenas("")).toEqual(0);
+    });
+});
+
+function calculadoraCadenas(cadena) {
+    return 0;
+}
