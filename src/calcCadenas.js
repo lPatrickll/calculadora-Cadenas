227720e48170376d45045943af02a673e0d297cf
Patrick <patrickgonzalesquintana@gmail.com>
2024-05-13 23:06:42 -0400

    Se mejoro la funcion sumarCadena para una cadena de dos numeros y me muestra la suma de los numeros de esa cadena

--- src/calcCadenas.js
+++ src/calcCadenas.js
@@ -1,8 +1,14 @@
 function sumarCadenas(cadena) {
+    let suma = 0;
     if (cadena === "")
-        return 0;
-    else
-        return parseInt(cadena);
+        return suma;
+    else {
+        const numerosCadena = cadena.split(",");
+        for (const numero of numerosCadena) {
+            suma += parseInt(numero);
+        }
+        return suma;
+    }
 }
 
 export default sumarCadenas;
--- src/calcCadenas.spec.js
+++ src/calcCadenas.spec.js
@@ -8,4 +8,8 @@ describe("Calculadora de Cadenas", () => {
     it("deberia mostrar la suma de una cadena con un solo numero", () => {
         expect(sumarCadenas("2")).toEqual(2);
     });
+
+    it("deberia mostrar la suma de una cadena de numeros separados por una coma como 2,3 que suma 5", () => {
+        expect(sumarCadenas("2,3")).toEqual(5);
+    });
 });
