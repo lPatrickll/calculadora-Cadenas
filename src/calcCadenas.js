e595f4d5cb9fcf514a446ca50b87900f07030bf8
Patrick <patrickgonzalesquintana@gmail.com>
2024-05-16 00:13:01 -0400

    Se hizo un refactorin para extraer los numeros de la cadena

--- src/calcCadenas.js
+++ src/calcCadenas.js
@@ -1,25 +1,34 @@
 function sumarCadenas(cadena) {
     let suma = 0;
-    if (cadena === "") {
-        return suma;
-    } else {
-        let numerosCadena;
-        const match = cadena.match(/\/\/\[(.)\]\s+(.*)/);
-        if (match) {
-            const caracterEspecial = match[1];
-            numerosCadena = match[2].split(new RegExp(`[${caracterEspecial},-]`));
-        } else {
-            numerosCadena = cadena.split(/,|-/);
-        }
 
-        for (const numero of numerosCadena) {
-            const numeroParseado = parseInt(numero);
-            if (numeroParseado <= 1000) {
-                suma += numeroParseado;
-            }
-        }
-        return suma;
+    const numerosCadena = obtenerNumeros(cadena);
+    const numerosValidos = filtrarNumerosValidos(numerosCadena);
+
+    for (const numero of numerosValidos) {
+        suma += parseInt(numero);
     }
+
+    return suma;
+}
+
+function obtenerNumeros(cadena) {
+    if (cadena === "") {
+        return [];
+    }
+
+    const match = cadena.match(/\/\/\[(.)\]\s+(.*)/);
+    if (match) {
+        const caracterEspecial = match[1];
+        return match[2].split(new RegExp(`[${caracterEspecial},-]`));
+    } else {
+        return cadena.split(/,|-/);
+    }
+}
+
+function filtrarNumerosValidos(numeros) {
+    return numeros
+        .map(numero => parseInt(numero))
+        .filter(numero => !isNaN(numero) && numero <= 1000);
 }
 
 export default sumarCadenas;
